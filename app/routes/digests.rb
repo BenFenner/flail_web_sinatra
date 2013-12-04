b060c932b69034127524445cd336162ada2abb62
Ben Fenner <bfenner@boardit.sc.gov>
2013-12-04 16:17:10 -0500

    Get the Prev/Next buttons working. Rough work on Resolve/Filter.

--- app/routes/digests.rb
+++ app/routes/digests.rb
@@ -1,14 +1,14 @@
 module FlailWeb
   class App < Sinatra::Base
   
-    get %r{/digests/([\h]+)} do
+    get '/digests/:digest_id?:offset?' do
       params[:offset] ||= 0
       resource
       haml 'digests/show'.to_sym
     end
     
     def resource
-      @resource ||= FlailException.with_digest(params[:captures].first).order('created_at desc').offset(params[:offset].to_i).first
+      @resource ||= FlailException.with_digest(params[:digest_id]).order('created_at desc').offset(params[:offset].to_i).first
     end
 
   end
