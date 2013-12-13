de0e268ef8a0da3fe9d5d2ecab5d1167d1673a8c
Ben Fenner <bfenner@boardit.sc.gov>
2013-12-13 15:38:16 -0500

    Only show unresolved exceptions in the main view.

--- app/routes/flail_web.rb
+++ app/routes/flail_web.rb
@@ -3,7 +3,7 @@ module FlailWeb
   
     get '/' do
       #authenticate_shim!
-      @collection = FlailException.within(18.days.ago)
+      @collection = FlailException.within(18.days.ago).unresolved
       @collection = @collection.tagged(params[:tagged]) unless params[:tagged].blank?
       haml :index
     end
