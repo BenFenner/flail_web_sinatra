14b74760815f16d0384ff5cb7984847b233147cc
Ben Fenner <bfenner@boardit.sc.gov>
2013-12-13 15:39:48 -0500

    Get the digests Resolve button to work.

--- app/routes/digests.rb
+++ app/routes/digests.rb
@@ -7,6 +7,13 @@ module FlailWeb
       haml 'digests/show'.to_sym
     end
     
+    post '/digests/:digest_id?:offset?' do
+      resource.resolve!
+      WebHook.trigger(:resolution, resource, url("digests/#{resource.digest}"))
+      flash[:notice] = "Resolved #{resource.occurrences.count} flailing exceptions: #{resource.class_name}"
+      redirect to('/')
+    end
+    
     def resource
       @resource ||= FlailException.with_digest(params[:digest_id]).order('created_at desc').offset(params[:offset].to_i).first
     end
