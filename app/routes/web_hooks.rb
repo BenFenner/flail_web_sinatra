1a031678abccc455c26a26cfe52fa7104c5cf863
Ben Fenner <bfenner@boardit.sc.gov>
2013-12-17 14:57:42 -0500

    Get web_hook viewing, creation, and deletion working.

--- app/routes/web_hooks.rb
+++ app/routes/web_hooks.rb
@@ -4,6 +4,23 @@ module FlailWeb
     get '/web_hooks' do
       haml 'web_hooks/index'.to_sym
     end
+    
+    post '/web_hooks' do
+      web_hook_attrs = params[:web_hook]
+      WebHook.create({:secure => web_hook_attrs[:secure], :url => web_hook_attrs[:url], :event => web_hook_attrs[:event]})
+      flash[:notice] = "Web hook was successfully created."
+      redirect to('/web_hooks')
+    end
+    
+    get '/web_hooks/new' do
+      haml 'web_hooks/new'.to_sym
+    end
+    
+    post '/web_hooks/:id' do
+      WebHook.find(params[:id]).destroy!
+      flash[:notice] = "Web hook was successfully destroyed."
+      redirect to('/web_hooks')
+    end
 
   end
 end
