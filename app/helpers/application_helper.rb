f41cd41290a6329f959e7a7d6eb0a1da59f5207a
Ben Fenner <bfenner@boardit.sc.gov>
2014-01-31 14:37:54 -0500

    Format the params, user, and env stuff better.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,3 +11,11 @@ def active?(tag = nil)
     ''
   end
 end
+
+def human_formatted_hash(input = {})
+  output = ""
+  input.each do |key, value|
+    output += "#{Rack::Utils.escape_html(key)}: #{Rack::Utils.escape_html(value)}<br><br>"
+  end
+  output
+end
