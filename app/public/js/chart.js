e3df78808d45663df58d8356308e480934cadc3a
Ben Fenner <bfenner@boardit.sc.gov>
2014-04-30 16:36:51 -0400

    Ugly relative and contextual placement to avoid use of centering, as Chrome and FF treat centering differently.

--- app/public/js/chart.js
+++ app/public/js/chart.js
@@ -2,11 +2,12 @@ $.getJSON(location.protocol + '//' + location.host + location.pathname + "chart_
 
   var maxNumExceptions = 100;
   var barWidth = 37;
+  var barPadding = 2;
   var barColor = "#F89406";
   var barHeight = 200;
   var xAxisPadding = 25;
   var height = barHeight + xAxisPadding;
-  var width = (barWidth + 2) * data.length;
+  var width = (barWidth + barPadding) * data.length;
 
   var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
   var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.exceptions; }) + 20]).
@@ -32,30 +33,46 @@ $.getJSON(location.protocol + '//' + location.host + location.pathname + "chart_
     data(data).
     enter().
     append("svg:text").
-    attr("x", function(datum, index) { return x(index) + (barWidth / 1.4); }).
+    attr("x", function(datum, index) {
+      if (datum.exceptions > 99) {
+        return x(index) + barWidth - 10;
+      } else if (datum.exceptions > 9) {
+        return x(index) + barWidth - 7;
+      }
+      return x(index) + barWidth - 4;
+    }).
     attr("y", function(datum) { 
       var yPos = barHeight - y(datum.exceptions);
       if (yPos > barHeight - 20) {
         yPos = barHeight - 20;
       }
-      return yPos; }).
+      return yPos;
+    }).
     attr("dx", -barWidth/2).
     attr("dy", "1.2em").
-    attr("text-anchor", "middle").
     text(function(datum) {
       if (datum.exceptions < maxNumExceptions) {
         return datum.exceptions;
       }
-      return (maxNumExceptions - 1) + "+" ;}).
+      return (maxNumExceptions - 1) + "+";
+    }).
     attr("fill", "grey");
 
   barDemo.selectAll("text.yAxis").
     data(data).
     enter().append("svg:text").
-    attr("x", function(datum, index) { return x(index) + (barWidth / 1.4); }).
+    attr("x", function(datum, index) {
+      if (datum.hour.length > 3) {
+        return x(index) + barWidth - 13;
+      } else if (datum.hour.length > 2) {
+        return x(index) + barWidth - 10;
+      } else if (datum.hour.length > 1) {
+        return x(index) + barWidth - 7;
+      }
+      return x(index) + barWidth - 5;
+    }).
     attr("y", barHeight).
     attr("dx", -barWidth/2).
-    attr("text-anchor", "middle").
     attr("style", "font-size: 12; font-family: Helvetica, sans-serif").
     text(function(datum) { return datum.hour;}).
     attr("transform", "translate(0, 18)").
