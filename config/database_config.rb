513500f21d817b03845bc4035fe4904232d043d3
Ben Fenner <bfenner@rfa.sc.gov>
2015-09-04 15:39:01 -0400

    I do believe I forgot to define the DB connection function properly.

--- config/database_config.rb
+++ config/database_config.rb
@@ -1,9 +1,11 @@
-db = URI.parse('postgres://user:password@host/flail_web_development')
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
+def establish_database_connection
+  db = URI.parse('postgres://user:password@host/flail_web_development')
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
